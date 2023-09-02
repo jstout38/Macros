9396f3355bd9aa955604611c0a7eef3f306e8842
jstout38 <jstout38@gmail.com>
2023-09-02 16:32:38 -0400

    fixed gitignore

--- build/config/prod.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    MongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
-};
