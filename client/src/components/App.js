6579b15f6c21965d689e9fc8feb834153dfdc89c
jstout38 <jstout38@gmail.com>
2023-09-08 20:17:04 -0400

    fetch user config

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 const Dashboard = () => React.createElement("h2", null, "Dashboard");
 const SurveyNew = () => React.createElement("h2", null, "SurveyNew");
 const Landing = () => React.createElement("h2", null, "Landing");
-const App = () => {
-    return (React.createElement("div", { className: "container" },
-        React.createElement(BrowserRouter, null,
-            React.createElement("div", null,
-                React.createElement(Header, null),
-                React.createElement(Route, { exact: true, path: "/", component: Landing }),
-                React.createElement(Route, { exact: true, path: "/surveys", component: Dashboard }),
-                React.createElement(Route, { path: "/surveys/new", component: SurveyNew })))));
-};
+class App extends Component {
+    render() {
+        return (React.createElement("div", { className: "container" },
+            React.createElement(BrowserRouter, null,
+                React.createElement("div", null,
+                    React.createElement(Header, null),
+                    React.createElement(Route, { exact: true, path: "/", component: Landing }),
+                    React.createElement(Route, { exact: true, path: "/surveys", component: Dashboard }),
+                    React.createElement(Route, { path: "/surveys/new", component: SurveyNew })))));
+    }
+    ;
+}
+;
 export default App;
--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
@@ -7,19 +7,21 @@ const Dashboard: React.FC = () => <h2>Dashboard</h2>
 const SurveyNew: React.FC = () => <h2>SurveyNew</h2>
 const Landing: React.FC = () => <h2>Landing</h2>
 
-const App: React.FC = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-         <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-         </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+    };
 };
 
 export default App;
