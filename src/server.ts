ffd0702a16b7a26ab1ec88865ae64be5cd8965a2
jstout38 <jstout38@gmail.com>
2023-09-24 18:15:02 -0400

    forgot to add redirect code to server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const keys = require('./config/keys');
 import mongoose from 'mongoose';
 import cookieSession from 'cookie-session';
-const passport = require('passport');
+import passport from 'passport';
 require('./models/User');
 require('./services/passport');
 
@@ -25,6 +25,14 @@ app.use(passport.session());
 //Import authentication routes
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 //Start server on port 5000
 app.listen(5000, () => {
   console.log("Server is up and running");
