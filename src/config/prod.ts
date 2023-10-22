ab365745be96b12886815015de58b0ceed543bac
jstout38 <jstout38@gmail.com>
2023-10-22 10:38:16 -0400

    updated production variables

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -2,5 +2,7 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   MongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  nutritionClientSecret: process.env.NUTRITION_CLIENT_SECRET,
+  nutritionClientID: process.env.NUTRITION_CLIENT_ID
 }
