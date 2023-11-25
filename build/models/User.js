f2efc1524b8f2dc134182e848f56ea6b57c8cae3
jstout38 <jstout38@gmail.com>
2023-11-25 16:05:16 -0500

    cleanup

--- build/models/User.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-//Create mongoose model for Users
-const userSchema = new mongoose_1.Schema({
-    googleId: { type: String, required: true },
-});
-//Create model on require 
-module.exports = (0, mongoose_1.model)('users', userSchema);
